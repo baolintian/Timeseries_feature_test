66853ac44347cbadcc94dced446bddf6dd4eb1e0
baolintian <tianbaolin1@gmail.com>
2021-11-30 15:31:46 +0800

    update data

--- timeseries_segment/segment.py
+++ timeseries_segment/segment.py
@@ -10,15 +10,27 @@ normal_std = 10
 # 认为一分钟内被分割的点都是在同一段变化的区间
 window_size = 60
 
-if __name__ == "__main__":
-    timeseries_name = "N00_KAA_0KAA00CP002@OUT.VAL"
-    config_path = "../config/" + timeseries_name
-    image_path = "../images/" + timeseries_name
-    timeseries_path = "../data/" + timeseries_name + ".csv"
-    trend_config, threshold_config, resample_frequency = read_config(config_path)
-    timeseries = read_timeseries(timeseries_path, str(resample_frequency) + "min")
+def sign(value, pre):
+    if value > 0:
+        return 1
+    elif value == 0:
+        return pre
+    else:
+        return 0
 
-    res_temp = pelt(normal_mean(pd.to_numeric(timeseries).values, 10), len(timeseries))
+def sign1(value):
+    if value > 0:
+        return 1
+    elif value < 0:
+        return -1
+    else:
+        return 0
+
+if __name__ == "__main__":
+    timeseries_path = "./tu_1.csv"
+    timeseries = read_timeseries(timeseries_path, str(120) + "min")
+
+    res_temp = pelt(normal_mean(pd.to_numeric(timeseries).values, 100), len(timeseries))
     res = []
 
     start = res_temp[0]
@@ -39,3 +51,21 @@ if __name__ == "__main__":
     if not used:
         res.append(start)
     print(res)
+    timeseries_value = timeseries.values
+    diff1 = []
+    diff2 = []
+    window_size = 10
+    for _, timeseries_value_ in enumerate(timeseries_value):
+        if _ == 0:
+            continue
+        diff2.append(sign1(timeseries_value[_] - timeseries_value[_ - 1]))
+        pre = sum(diff2[max(0, _-window_size): _-1])
+        if pre > 0:
+            pre = 1
+        else:
+            pre = 0
+        diff1.append(sign(timeseries_value[_]-timeseries_value[_-1], pre))
+
+
+    plt.scatter(range(len(diff1)), diff1)
+    plt.show()
